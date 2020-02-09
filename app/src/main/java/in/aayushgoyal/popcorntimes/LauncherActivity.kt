634484a8e6583c75d5a7f86cce753c07724cf6e9
Aayush Goyal <aayushgoyal.mps@gmail.com>
2020-02-09 17:49:51 +0530

    First commit.
    Signed-off-by: Aayush Goyal <aayushgoyal.mps@gmail.com>

---
+++ app/src/main/java/in/aayushgoyal/popcorntimes/LauncherActivity.kt
@@ -0,0 +1,12 @@
+package `in`.aayushgoyal.popcorntimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
