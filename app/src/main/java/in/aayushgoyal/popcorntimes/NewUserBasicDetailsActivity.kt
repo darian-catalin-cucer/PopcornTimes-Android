ad26771d363fd8d65fac6e85c735628d36c1a1a0
Aayush Goyal <aayushgoyal.mps@gmail.com>
2020-03-02 15:49:38 +0530

    v0-PPTA-4.1-1: UI - Render the designed UI with code.
    Signed-off-by: Aayush Goyal <aayushgoyal.mps@gmail.com>

---
+++ app/src/main/java/in/aayushgoyal/popcorntimes/NewUserBasicDetailsActivity.kt
@@ -0,0 +1,12 @@
+package `in`.aayushgoyal.popcorntimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUserBasicDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user_basic_details)
+    }
+}
